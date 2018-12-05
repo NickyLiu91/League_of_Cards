58d13c2517b4c5b50ad426a5c9f533d6307e90dd
github username <github email address>
2018-12-05 11:22:42 -0500

    added graveyard abilities

--- frontend/league-of-cards/src/components/DuelCard.js
+++ frontend/league-of-cards/src/components/DuelCard.js
@@ -6,9 +6,21 @@ const DuelCard = (props) => {
       <div className="duel-card" >
       </div>
     )
+  } else if(props.duelCard.position != undefined){
+    return(
+      <div className={`duel-card ${props.duelCard.position}`} onClick={event => props.clickAction(props.duelCard)}>
+        <div>Name: {props.duelCard.name}, {props.duelCard.title}</div>
+        <img className="duel-card-image" src={'image/' + props.duelCard.image}/>
+        <br/>
+        <div>Role: {props.duelCard.role}</div>
+        <div>Attack: {props.duelCard.attack}</div>
+        <div>Magic: {props.duelCard.magic}</div>
+        <div>Defense: {props.duelCard.defense}</div>
+      </div>
+    )
   } else {
     return(
-      <div className="duel-card" onClick={event => {props.clickAction(props.duelCard)}}>
+      <div className="duel-card" onClick={event => props.clickAction(props.duelCard)}>
         <div>Name: {props.duelCard.name}, {props.duelCard.title}</div>
         <img className="duel-card-image" src={'image/' + props.duelCard.image}/>
         <br/>
