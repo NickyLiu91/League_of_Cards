6cb17c29a8ca8e222c7cacb2719f76b963a4a37d
github username <github email address>
2019-02-09 18:34:16 -0500

    finished player deck gen and auto decklist sort

--- frontend/league-of-cards/src/containers/SideBar.js
+++ frontend/league-of-cards/src/containers/SideBar.js
@@ -5,7 +5,27 @@ let i = 1
 export default class SideBar extends React.Component {
 
   generateCards = () => {
-    return this.props.currentDeckCards.map(
+
+    let sortedDeck = this.props.currentDeckCards.sort( (a, b) => {
+      if (a.cardtype === 'Champion' && b.cardtype === 'Ability') {
+        return -1
+      } else if (a.cardtype === 'Champion' && b.cardtype === 'Item') {
+        return -1
+      } else if (a.cardtype === 'Ability' && b.cardtype === 'Item') {
+        return -1
+      } else if (a.cardtype === b.cardtype) {
+        if (a.name > b.name) {
+          return 1
+        } else {
+          return -1
+        }
+      } else {
+        return 1
+      }
+      return 0
+    })
+
+    return sortedDeck.map(
       cardObj => <SideBarCard key={i++} card={cardObj}
       removeFromDeck={this.props.removeFromDeck}/>
     )
