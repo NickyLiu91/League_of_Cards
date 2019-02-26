8966a0de64ef8f92e53781e54deaf3c0e2dbbb71
github username <github email address>
2019-02-26 06:44:16 -0500

    seperated log-in screen into component

---
+++ frontend/league-of-cards/src/components/HomeScreen.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const HomeScreen = (props) => {
+  if (props.loggedIn === false) {
+    return(
+        <div>
+        <img id="shurima" src="image/shurima_sun_01.jpg" />
+          <div id="home">
+          <h1>LEAGUE OF CARDS</h1>
+            <form>
+              <h1>Log-In</h1>
+                Account: <input type="text" value={props.name} onChange={event => props.handleName(event)}/>
+                <br/>
+                <br/>
+                Password: <input type="password" value={props.password} onChange={event => props.handlePassword(event)}/>
+                <br/>
+                <br/>
+                <button type="button" onClick={event => props.getPlayer(event)}>Submit</button>
+            </form>
+              <br/>
+            <button className="create" onClick={event => {props.renderStuff(event)}}>Create Account</button>
+          </div>
+        </div>
+      )
+  } else if (props.loggedIn === true) {
+    return(
+      <div>
+        <img id="demacia" src="image/demacia.jpeg" />
+        <div id="logged-in">
+          <h1>LEAGUE OF CARDS</h1>
+            <h1>Welcome, {props.currentPlayer.name}!</h1>
+            <br/>
+            <button className="campaign" onClick={event => {props.renderStuff(event)}}>Campaign</button>
+            <br/>
+            <br/>
+            <button className="duelistsList" onClick={event => {props.renderStuff(event)}}>DUEL!!!</button>
+            <br/>
+            <br/>
+            <button className="collection" onClick={event => {props.renderStuff(event)}}>Collection</button>
+            <br/>
+            <br/>
+            <button className="store" onClick={event => {props.renderStuff(event)}}>Card Store</button>
+            <br/>
+            <br/>
+            <button className="decksList" onClick={event => {props.renderStuff(event)}}>Decks</button>
+            <br/>
+            <br/>
+            <button onClick={props.logOut}>Log-Out</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HomeScreen;
