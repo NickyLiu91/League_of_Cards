ecad4ba59d7927ac12e0fa29de904c853f1bd242
NickyLiu91 <nickyliu91@gmail.com>
2019-03-02 19:33:50 -0500

    fixed campaign dialogue duel display

--- frontend/league-of-cards/src/containers/Home.js
+++ frontend/league-of-cards/src/containers/Home.js
@@ -2259,8 +2259,8 @@ export default class Home extends React.Component {
                 computer: false,
                 level: "1-1",
                 gold: 100,
-                dialogue: 317,
-                defeated_id: 23,
+                dialogue: 0,
+                defeated_id: 0,
                 completed: false
               }
         )}).then(res => {
