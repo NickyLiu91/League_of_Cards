29b3fb843bc5c8d2e788844935ee66fd57b2d8f4
NickyLiu91 <nickyliu91@gmail.com>
2020-02-12 06:58:07 -0500

    adding and removing cards from collection and side bar with redux complete

--- frontend/league_of_cards/src/containers/SideBar.js
+++ frontend/league_of_cards/src/containers/SideBar.js
@@ -10,20 +10,21 @@ class SideBar extends React.Component {
     fetch(`http://localhost:3000/api/v1/deckcards`)
     .then(res => res.json())
     .then(json => deckCardToDelete = json.filter(
-      obj => obj.card.id === card.id && obj.deck.id === this.state.currentDeck.id
+      obj => obj.card.id === card.id && obj.deck.id === this.props.deck.id
     )[0])
     .then(response => {
       this.setState({
-        currentDeckCards: this.state.currentDeckCards.filter(cardObj => cardObj.id !== card.id)
+        currentDeckCards: this.props.deckCards.filter(cardObj => cardObj.id !== card.id)
       }, () => {fetch(`http://localhost:3000/api/v1/deckcards/${deckCardToDelete.id}`, {
         method: 'DELETE'
       })})
     })
+    this.props.changeDeckCards(this.props.deckCards.filter(cardObj => cardObj.id !== card.id))
   }
 
   generateCards = () => {
 
-    let sortedDeck = this.props.currentDeckCards.sort( (a, b) => {
+    let sortedDeck = this.props.deckCards.sort( (a, b) => {
       if (a.cardtype === 'Champion' && b.cardtype === 'Ability') {
         return -1
       } else if (a.cardtype === 'Champion' && b.cardtype === 'Item') {
@@ -44,7 +45,7 @@ class SideBar extends React.Component {
 
     return sortedDeck.map(
       cardObj => <SideBarCard key={cardObj.id} card={cardObj}
-      removeFromDeck={this.props.removeFromDeck}/>
+      removeFromDeck={this.removeFromDeck}/>
     )
   }
 
@@ -61,41 +62,21 @@ class SideBar extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    account: state.accountChanger.account,
-    name: state.nameChanger.name,
-    cards: state.cardsChanger.cards,
     currentPlayerCards: state.currentPlayerCardsChanger.currentPlayerCards,
     noDupesCurrentPlayerCards: state.noDupesCurrentPlayerCardsChanger.noDupesCurrentPlayerCards,
     deck: state.deckChanger.deck,
     deckCards: state.deckCardsChanger.deckCards,
-    characters: state.charactersChanger.characters,
-    enemy: state.enemyChanger.enemy,
-    card: state.cardChanger.card,
-    decksList: state.decksListChanger.decksList,
-    gold: state.goldChanger.gold,
-    defeated: state.defeatedChanger.defeated,
-    dialogue: state.dialogueChanger.dialogue,
-    location: state.locationChanger.location,
+    card: state.cardChanger.card
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    changeAccount: (event) => dispatch({type: 'CHANGE_ACCOUNT', newAccount: event}),
-    changeName: (event) => dispatch({type: 'CHANGE_NAME', newName: event}),
-    changeCards: (event) => dispatch({type: 'CHANGE_CARDS', newCards: event}),
     changeCurrentPlayerCards: (event) => dispatch({type: 'CHANGE_CURRENTPLAYERCARDS', newCurrentPlayerCards: event}),
     changeNoDupesCurrentPlayerCards: (event) => dispatch({type: 'CHANGE_NODUPESCURRENTPLAYERCARDS', newNoDupesCurrentPlayerCards: event}),
     changeDeck: (event) => dispatch({type: 'CHANGE_DECK', newDeck: event}),
     changeDeckCards: (event) => dispatch({type: 'CHANGE_DECKCARDS', newDeckCards: event}),
-    changeCharacters: (event) => dispatch({type: 'CHANGE_CHARACTERS', newCharacters: event}),
-    changeEnemy: (event) => dispatch({type: 'CHANGE_ENEMY', newEnemy: event}),
-    changeCard: (event) => dispatch({type: 'CHANGE_CARD', newCard: event}),
-    changeDecksList: (event) => dispatch({type: 'CHANGE_DECKSLIST', newDecksList: event}),
-    changeGold: (event) => dispatch({type: 'CHANGE_GOLD', newGold: event}),
-    changeDefeated: (event) => dispatch({type: 'CHANGE_DEFEEATED', newDefeated: event}),
-    changeDialogue: (event) => dispatch({type: 'CHANGE_DIALOGUE', newDialogue: event}),
-    changeLocation: (event) => dispatch({type: 'CHANGE_LOCATION', newLocation: event}),
+    changeCard: (event) => dispatch({type: 'CHANGE_CARD', newCard: event})
   }
 }
 
