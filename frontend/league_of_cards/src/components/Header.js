0e61540b4a0133ce98bc6be38c7ca1c2db9de60f
NickyLiu91 <nickyliu91@gmail.com>
2020-02-21 00:07:42 -0500

    added deck length checker to header

--- frontend/league_of_cards/src/components/Header.js
+++ frontend/league_of_cards/src/components/Header.js
@@ -1,21 +1,41 @@
 import React from "react";
 import {connect} from 'react-redux';
-// import {compose} from 'redux';
+import {compose} from 'redux';
 import {Route, Link, withRouter, NavLink} from 'react-router-dom';
 
-const Header = (props) => {
+class Header extends React.Component {
+
+  checkDeckSize = (string) => {
+    if (this.props.deckCards.length < 40) {
+      alert ('Your deck has less than 40 cards!')
+    } else {
+      this.props.history.push(string)
+    }
+  }
+
+  render() {
     return(
       <nav>
-          <ul>
-            <li className="home" onClick={() => {props.history.push("/")}}>Home</li>
-            <li className="campaign" onClick={() => {props.history.push("/campaign")}}>Campaign</li>
-            <li className="duelistsList" onClick={() => {props.history.push("/duelistsList")}}>Free Duel</li>
-            <li className="collection" onClick={() => {props.history.push("/collection")}}>Collection</li>
-            <li className="store" onClick={() => {props.history.push("/cardStore")}}>Store</li>
-            <li className="decksList" onClick={() => {props.history.push("/decksList")}}>Decks</li>
-          </ul>
+      <ul>
+      <li className="home" onClick={() => {this.props.history.push("/")}}>Home</li>
+      <li className="campaign" onClick={() => {this.checkDeckSize("/campaign")}}>Campaign</li>
+      <li className="duelistsList" onClick={() => {this.checkDeckSize("/duelistsList")}}>Free Duel</li>
+      <li className="collection" onClick={() => {this.props.history.push("/collection")}}>Collection</li>
+      <li className="store" onClick={() => {this.props.history.push("/cardStore")}}>Store</li>
+      <li className="decksList" onClick={() => {this.props.history.push("/decksList")}}>Decks</li>
+      </ul>
       </nav>
     )
+  }
 }
 
-export default withRouter(Header)
+const mapStateToProps = state => {
+  return {
+    deckCards: state.deckCardsChanger.deckCards
+  }
+}
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps)
+)(Header);
